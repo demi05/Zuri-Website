c08dd04d3a3e66f933ee514c3cdea1d05339dded
demi05 <demiladeleshi@gmail.com>
2022-10-17 19:01:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const btnHeader = document.querySelector(".btn-header");
+const hireTalents = document.querySelector(".hire-talents");
+
+btnHeader.addEventListener("click",()=>{
+    hireTalents.classList.toggle("active");
+})
